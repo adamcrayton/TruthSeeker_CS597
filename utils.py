e221fe197fd89a7e2d9ba63edbc1d346e435d43f
Akash Patel <akashpatel@Akashs-MacBook-Air.local>
2023-12-01 20:18:50 -0500

    Prediction script added

--- utils.py
+++ utils.py
@@ -1,32 +1,37 @@
 from transformers import BertForSequenceClassification,RobertaForSequenceClassification, DistilBertForSequenceClassification, AlbertForSequenceClassification
-def get_model(model_name, num_classes):
+def get_model(model_name, num_classes, model_path = None):
+    
     if model_name == 'roberta': 
+        model_path = model_path if model_path else "cardiffnlp/twitter-roberta-base-emotion"
         model = RobertaForSequenceClassification.from_pretrained(
-            "cardiffnlp/twitter-roberta-base-emotion",
+            model_path,
             num_labels = num_classes,
             output_attentions = False,
             output_hidden_states = False,
             ignore_mismatched_sizes=True
         )
     elif model_name == 'distilbert':
+        model_path = model_path if model_path else "distilbert-base-uncased"
         model = DistilBertForSequenceClassification.from_pretrained(
-            "distilbert-base-uncased",
+            model_path,
             num_labels = num_classes,
             output_attentions = False,
             output_hidden_states = False,
             ignore_mismatched_sizes=True
         )
     elif model_name == 'albert':
+        model_path = model_path if model_path else "textattack/albert-base-v2-imdb"
         model = AlbertForSequenceClassification.from_pretrained(
-            "textattack/albert-base-v2-imdb",
+            model_path,
             num_labels = num_classes,
             output_attentions = False,
             output_hidden_states = False,
             ignore_mismatched_sizes=True
         )
     else:
+        model_path = model_path if model_path else "bert-base-uncased"
         model = BertForSequenceClassification.from_pretrained(
-            "bert-base-uncased", # Use the 12-layer BERT model, with an uncased vocab.
+            model_path, # Use the 12-layer BERT model, with an uncased vocab.
             num_labels = num_classes, # The number of output labels--2 for binary classification.
                             # You can increase this for multi-class tasks.   
             output_attentions = False, # Whether the model returns attentions weights.
