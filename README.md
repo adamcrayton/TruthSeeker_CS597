73f18e8b07a50fc15db3a247233897378a8b4f13
Jeremiah Robinson <jeremiahrobinson842@u.boisestate.edu>
2024-11-22 22:45:38 -0700

    Nuked Repository

--- README.md
+++
@@ -1,5 +0,0 @@
-# Truthseeker
-
-This is a project done by Adam Crayton and Jeremiah Robinson.
-
-Paper is attached here: 
--- push_wandb_table.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import wandb
-
-df = pd.read_csv('/Users/akashpatel/Tokyo.csv')
-run = wandb.init(project="PeopleFlow", job_type="Logging table")
-
-table = wandb.Table(data=df)
-run.log({"table_key": table})
